14e03cfe8de335dcdaa01bccfdd6e5189c329061
josave <josave@odds.team>
2023-08-08 14:34:06 +0700

    Feat: mapping mock data

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-data';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero:Hero = {
-    id: 1,
-    name: 'Windstorm'
+  //set new value from mock data
+  heroes = HEROES;
+
+  //set empty object for instead of selected value  
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
