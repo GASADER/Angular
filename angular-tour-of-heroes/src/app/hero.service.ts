502b2f108be64dbb1114f3e10471fc004232960f
josave <josave@odds.team>
2023-08-09 14:32:27 +0700

    Add: error handler

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-data';
 import { Observable, of } from 'rxjs';
+import { catchError,map,tap } from 'rxjs/operators';
+
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { MessageService } from './messages.service';
 @Injectable({
@@ -18,10 +20,15 @@ export class HeroService {
     private messageService: MessageService
     ) {}
 
-  //method for count call service
+  //service for call all data
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes',[]))
+    )
   }
+
+
   getHero(id:Number): Observable<Hero>{
     const hero = HEROES.find(h => h.id === id)!
     this.messageService.add(`HeroService: fetched hero id=${id}`)
@@ -30,5 +37,13 @@ export class HeroService {
 
   private log(messages: string){
     this.messageService.add(`HeroService: ${messages}`)
+  }
+
+  private handleError<T>(operation= 'operation',result?: T){
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.messages}`)
+      return of(result as T)
+    }
   }
 }
