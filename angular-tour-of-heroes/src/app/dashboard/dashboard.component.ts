e30f4fa3eb11ad35f5387f6faf9234ada965c4a4
josave <josave@odds.team>
2023-08-09 13:44:01 +0700

    Fix: debug map data dashboard

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 @Component({
-  selector: 'app-dashborad',
+  selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
@@ -15,6 +15,6 @@ export class DashboardComponent implements OnInit {
   }
 
   getHeroes(): void{
-    this.heroService.getHeroes().subscribe(heroes => this.heroes.slice(1,5))
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1,5))
   }
 }
