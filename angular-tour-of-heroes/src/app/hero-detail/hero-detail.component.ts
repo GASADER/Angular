5b86f95f38e7cef54e1f78c44775d654adf85af6
josave <josave@odds.team>
2023-08-09 13:28:52 +0700

    Feat: call detail from hero component

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
+export class HeroDetailComponent implements OnInit {
+  // add constructor
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+  
+  ngOnInit(): void {
+      this.getHero()
+  }
 
-export class HeroDetailComponent {
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
   //get decorator for hero value from parent component
-  @Input() hero? : Hero;
+  @Input() hero?: Hero;
 }
